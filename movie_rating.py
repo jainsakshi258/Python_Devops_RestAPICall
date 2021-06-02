64a52bdb5a1ee5b704a25d122e76fc4959b5b594
jainsakshi258 <72534515+jainsakshi258@users.noreply.github.com>
2021-06-02 22:31:39 +0530

    Add files via upload

---
+++ movie_rating.py
@@ -0,0 +1,19 @@
+import requests
+class MovieRatings:
+    @staticmethod
+    def get_movie_rating_by_name(movie_name):
+        url = f'http://www.omdbapi.com/?t={movie_name}&plot=full&apikey=cf59f550'
+
+        response = requests.get(url, ).json()
+
+        for rating in response['Ratings']:
+            if rating['Source'] == 'Rotten Tomatoes':
+                return rating['Value']
+        return "No Rating Found"
+
+    @staticmethod
+    def get_movie_rating():
+        movie_name = input()
+        movie_rating = MovieRatings.get_movie_rating_by_name(movie_name)
+        print(movie_rating)
+
